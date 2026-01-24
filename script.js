b9497b5c988b4da1909b2cc4d2443d42b5e4dcdd
Pedro Alves <pedroalves16.dev@gmail.com>
2026-01-24 10:31:51 -0300

    corrigind bugs na renderização de tarefas

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const inputTarefa = document.getElementById('input-tarefa');
 const botaoAdicionar = document.getElementById('botao-adicionar');
 const listaTarefas = document.querySelector('.lista-tarefas'); //ul
-
 //
 
 lookForTasks();
 
 botaoAdicionar.addEventListener('click', (e) => {
+    const valorTarefa = inputTarefa.value;
     e.preventDefault();
 
-    if(inputTarefa.value === '') return;
-
-    criaTarefa();
+    if (inputTarefa.value === '') return;
+    criaTarefa(valorTarefa);
     salvarTarefaNoBanco();
 
     inputTarefa.value = '';
@@ -20,34 +19,35 @@ botaoAdicionar.addEventListener('click', (e) => {
 
 
 inputTarefa.addEventListener('keypress', e => {
-    
-    if(e.code === 'Enter'){
-        
-    if(inputTarefa.value === '') return;
+    const valorTarefa = inputTarefa.value;
+    if (e.code === 'Enter') {
 
-    criaTarefa();
-    salvarTarefaNoBanco();
+        if (inputTarefa.value === '') return;
 
-    inputTarefa.value = '';
+        criaTarefa(valorTarefa);
+        salvarTarefaNoBanco();
+
+        inputTarefa.value = '';
     }
 })
 
-function criaTarefa(tarefa){
-    const valorTarefa = inputTarefa.value;
+function criaTarefa(Conteudotarefa) {
 
     const li = document.createElement('li');
     li.classList.add('tarefas-item');
-    li.innerHTML = ` <div> <input type = 'checkbox' class="input-caixa"><span>${valorTarefa}</span> </div> <button class="botao-apagar"> <img src="trash-2 (1).svg" alt=""> </button>`;
-    
+    li.innerHTML = ` <div> <input type = 'checkbox' class="input-caixa"><span>${Conteudotarefa}</span> </div> <button class="botao-apagar"> <img src="trash-2 (1).svg" alt=""> </button>`;
+
     listaTarefas.appendChild(li);
     lookForTasks();
+
+    return li;
 }
 
 listaTarefas.addEventListener('change', (e) => {
-    if(e.target.type === 'checkbox'){
+    if (e.target.type === 'checkbox') {
         const li = e.target.parentElement;
-        
-        if(e.target.checked){
+
+        if (e.target.checked) {
             li.classList.add('item-text');
             salvarTarefaNoBanco();
         } else {
@@ -64,7 +64,7 @@ listaTarefas.addEventListener('change', (e) => {
 listaTarefas.addEventListener('click', e => {
     const botaoClicado = e.target.closest('.botao-apagar');
 
-    if(botaoClicado){
+    if (botaoClicado) {
         botaoClicado.parentElement.remove();
         lookForTasks();
         salvarTarefaNoBanco();
@@ -78,14 +78,14 @@ function salvarTarefaNoBanco(tarefaCriada) {
     const tarefasDB = [];
 
 
-    for (tarefa of arrayLi){
+    for (tarefa of arrayLi) {
         const div = tarefa.querySelector('div');
         tarefasDB.push({
             task: tarefa.innerText,
             done: div.classList.contains('item-text')
         });
 
-        
+
     }
 
     const tarefasJson = JSON.stringify(tarefasDB);
@@ -94,30 +94,29 @@ function salvarTarefaNoBanco(tarefaCriada) {
 
 function carregartarefasSalvas() {
     let tarefas = localStorage.getItem('tarefa');
+
     const listaTarefasSalvas = JSON.parse(tarefas);
 
-    for (tarefa of listaTarefasSalvas){
-    const li = document.createElement('li');
-    li.classList.add('tarefas-item');
-    li.innerHTML = ` <div> <input type = 'checkbox' class="input-caixa"><span>${tarefa.task}</span> </div> <button class="botao-apagar"> <img src="trash-2 (1).svg" alt=""> </button>`;
+    for (tarefa of listaTarefasSalvas) {
 
-    const inputCaixa = li.querySelector('input');
+        const li = criaTarefa(tarefa.task);
 
-    if(tarefa.done === true){
-        inputCaixa.checked = true;
-        li.classList.add('item-text');
-    }
-    listaTarefas.appendChild(li);
-    lookForTasks();
+        const inputCaixa = li.querySelector('input');
+
+        if (tarefa.done === true) {
+            inputCaixa.checked = true;
+            li.classList.add('item-text');
+        }
+        listaTarefas.appendChild(li);
+        lookForTasks();
     }
 }
 
-function lookForTasks(){ //verifica se existem itens dentro da lista e exibe a div de tarefa vazia
+function lookForTasks() { //verifica se existem itens dentro da lista e exibe a div de tarefa vazia
     const primeiroItem = listaTarefas.querySelector('li');
-    console.log(primeiroItem);
     const nenhumaTarefa = document.querySelector('.tarefasVazias');
 
-    if(primeiroItem){
+    if (primeiroItem) {
         nenhumaTarefa.style.display = 'none'
     } else {
         nenhumaTarefa.style.display = 'block';
