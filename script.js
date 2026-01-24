434d138f6be0c8e9b053dee40b0e39e626dd2e25
Pedro Alves <pedroalves16.dev@gmail.com>
2026-01-24 10:15:27 -0300

    corrigindo bug de sobrescrever localstorage no f5

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const inputTarefa = document.getElementById('input-tarefa');
 const botaoAdicionar = document.getElementById('botao-adicionar');
-const listaTarefas = document.querySelector('.lista-tarefas');
+const listaTarefas = document.querySelector('.lista-tarefas'); //ul
 
 //
+
+lookForTasks();
 
 botaoAdicionar.addEventListener('click', (e) => {
     e.preventDefault();
 
     if(inputTarefa.value === '') return;
-
-    const primeiroItem = listaTarefas.firstChild;
-    if(primeiroItem){
-        const nenhumaTarefa = document.querySelector('.tarefasVazias');
-        nenhumaTarefa.style.display = 'none';
-    }
 
     criaTarefa();
     salvarTarefaNoBanco();
@@ -21,20 +17,17 @@ botaoAdicionar.addEventListener('click', (e) => {
     inputTarefa.value = '';
 })
 
+
+
 inputTarefa.addEventListener('keypress', e => {
     
     if(e.code === 'Enter'){
         
     if(inputTarefa.value === '') return;
 
-    const primeiroItem = listaTarefas.firstChild;
-    if(primeiroItem){
-        const nenhumaTarefa = document.querySelector('.tarefasVazias');
-        nenhumaTarefa.style.display = 'none';
-    }
-
     criaTarefa();
     salvarTarefaNoBanco();
+
     inputTarefa.value = '';
     }
 })
@@ -47,7 +40,7 @@ function criaTarefa(tarefa){
     li.innerHTML = ` <div> <input type = 'checkbox' class="input-caixa"><span>${valorTarefa}</span> </div> <button class="botao-apagar"> <img src="trash-2 (1).svg" alt=""> </button>`;
     
     listaTarefas.appendChild(li);
-
+    lookForTasks();
 }
 
 listaTarefas.addEventListener('change', (e) => {
@@ -60,7 +53,10 @@ listaTarefas.addEventListener('change', (e) => {
         } else {
             li.classList.remove('item-text');
             salvarTarefaNoBanco();
+            lookForTasks();
         }
+
+        lookForTasks();
 
     }
 })
@@ -70,10 +66,9 @@ listaTarefas.addEventListener('click', e => {
 
     if(botaoClicado){
         botaoClicado.parentElement.remove();
+        lookForTasks();
         salvarTarefaNoBanco();
-        if(listaTarefas.children.length === 0){
-            document.querySelector('.tarefasVazias').style.display = 'block';
-        }
+
     }
 })
 
@@ -89,6 +84,8 @@ function salvarTarefaNoBanco(tarefaCriada) {
             task: tarefa.innerText,
             done: div.classList.contains('item-text')
         });
+
+        
     }
 
     const tarefasJson = JSON.stringify(tarefasDB);
@@ -98,13 +95,6 @@ function salvarTarefaNoBanco(tarefaCriada) {
 function carregartarefasSalvas() {
     let tarefas = localStorage.getItem('tarefa');
     const listaTarefasSalvas = JSON.parse(tarefas);
-
-    if(tarefas){
-        document.querySelector('.tarefasVazias').style.display = 'none';
-    } else {
-        document.querySelector('.tarefasVazias').style.display = 'block';
-
-    }
 
     for (tarefa of listaTarefasSalvas){
     const li = document.createElement('li');
@@ -117,9 +107,22 @@ function carregartarefasSalvas() {
         inputCaixa.checked = true;
         li.classList.add('item-text');
     }
-    salvarTarefaNoBanco();
     listaTarefas.appendChild(li);
+    lookForTasks();
     }
 }
+
+function lookForTasks(){ //verifica se existem itens dentro da lista e exibe a div de tarefa vazia
+    const primeiroItem = listaTarefas.querySelector('li');
+    console.log(primeiroItem);
+    const nenhumaTarefa = document.querySelector('.tarefasVazias');
+
+    if(primeiroItem){
+        nenhumaTarefa.style.display = 'none'
+    } else {
+        nenhumaTarefa.style.display = 'block';
+    }
+}
+
 carregartarefasSalvas();
 
